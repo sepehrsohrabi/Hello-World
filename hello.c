d7716fc17fe119371fc215e8fae2a8e927c41061
Sepehr Sohrabi <31166217+sepehrsohrabi@users.noreply.github.com>
2018-05-25 18:33:46 +0430

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,8 @@
+#
+int main () {
+	printf("Hello World!\nThis is just a test and nothing more \nwhat R U Looking 4? \nPress Enter Key to Get the Fuck out of here :|\n");
+	printf("No Seriously GET OUT!");
+	getchar();
+	return 0;
+}
+
